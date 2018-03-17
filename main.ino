73e3857aaf5928113b58ac0db6f34409a0458ba0
Darius Zhu <dzhu5718@uni.sydney.edu.au>
2018-03-17 18:51:55 +1100

    Cleaned Comments + Bug fixing
    Cleaned documentation. Assigned pin numbers for diode. Changed serial.read() to .write() to save processing power

--- main.ino
+++ main.ino
@@ -1,68 +1,44 @@
 /*PSEUDOCODE
 
-//variables
-Int count = 0;
-
-Void setup(){
-  //we need to set the laser as an output
-  // X is a pin within a specific range -> need to check documentation for specific board
-  pinMode(X,OUTPUT);
-  Serial.begin(9600);
-}
-
-lightFlag = false;
-
-Void loop(){
-  while(1){
-   //turn the laser on
-   digitalWrite(X,HIGH);
-  /*1. read the photoresistor
-  2. IF STATEMENT
-  	A. value is above a certain threshold, no change -> go to end of loop
-    if (lightFlag) {
-      count++;
-      lightFlag = false;
-    }
-  	B. if value is below a certain threshold
-		  I. keep on reading the values on photoresistor
-      lightFlag = true;
-    Repeat loop
-  }
-}
-
-
 // Link for more advanced wire usage
 https://create.arduino.cc/projecthub/ianabcumming/arduino-laser-tripwire-31e473?ref=search&ref_id=laser&offset=4*/
 
-// Socket library may need to be imported
 int count = 0;
 boolean isLight = false;
 
 void setup() {
-  // Set laser up
-  pinMode(11, OUTPUT);
-  pinMode(A0, INPUT);
+	// Activate diode on pin 9
+	pinMode(9, OUTPUT);
 
-  Serial.begin(9600);
+	// Activate photoresistor
+	pinMode(A0, INPUT);
+
+	// Begin communication with serial port
+	Serial.begin(9600);
 }
-
-
 
 void loop(){
 	while(1){
-   //turn the laser on
-		digitalWrite(12, HIGH);
+		// Turn the laser on
+		digitalWrite(9, HIGH);
+
 		// This reads a value from 0 - 1024
 		double lightAnalog = analogRead(A0);
-		if (lightAnalog > 800)
+
+		// Sensor unblocked
+		if (lightAnalog > 800) {
+
+			// Sensor was previously blocked
 			if (isLight) {
-				Serial.print("a");
+				Serial.write(0);
 				isLight = false;
 			}
 			delay (200);
 			continue;
-		else {
-			isLight = true;
+
+		// Sensor blocked
+		} else {
+				isLight = true;
 		}
 		delay (200);
 	}
